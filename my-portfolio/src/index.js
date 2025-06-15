2524efb3bec4572ab6164b1b49e529708e53d191
proxiee <yashwanthtirupati99@gmail.com>
2025-06-15 00:31:34 -0400

    Prepare for Gihub Pages Deployment

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* 2. Wrap your App component with BrowserRouter */}
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
